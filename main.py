8ea9ab94b6d07f4ed77190d57ddc89d887eb42dd
keita.midorikawa <keita.midorikawa@beex-inc.com>
2021-12-29 19:21:35 +0900

    OAuth2 with Password and Bearer認証

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic.schema import schema
 from sqlalchemy.orm import Session
 import crud, models, schemas
 from database import SessionLocal, engine
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
 
 models.Base.metadata.create_all(bind=engine)
+
+# OAuthPasswordBearerインスタンスであると同時に、oauth2_scheme(some, parameters)のように呼び出し可能なので
+# Depends(oauth2_scheme)と書くことができる
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -43,6 +49,41 @@ def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = D
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 @app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    print(token)
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+@app.post("/token")
+def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    # OAuth2PasswordRequestForm には
+    db_user = crud.get_user_by_email(db, form_data.username)
+    if db_user is None:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    hashed_password = form_data.password + "notreallyhashed"
+    if hashed_password != db_user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    # tokenエンドポイントの戻り値はaccess_tokenとtoken_typeを含むdictである必要がある
+    # https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#return-the-token
+    return {"access_token": db_user.email, "token_type": "bearer"}
+
+
+
+def get_current_active_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    # tokenには "/token" でリターンした access_token が格納されている
+    email = token
+    db_user = crud.get_user_by_email(db, email)
+    if db_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    if not db_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return db_user
+
+
+@app.get("/myuser", response_model=schemas.User)
+def read_users_me(current_user: models.User = Depends(get_current_active_user)):
+    return current_user
