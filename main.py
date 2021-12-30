b30b58a78317133875f4bda0354e6c5f58c2cfae
keita.midorikawa <keita.midorikawa@beex-inc.com>
2021-12-30 18:53:44 +0900

    jwt認証

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-from typing import List
+from typing import List, Optional
+from datetime import timedelta
 
 from fastapi import Depends, FastAPI, HTTPException, status
-from pydantic.schema import schema
-from sqlalchemy.orm import Session
-import crud, models, schemas
 from database import SessionLocal, engine
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+
+import crud, models, schemas, auth
+
 
 
 models.Base.metadata.create_all(bind=engine)
-
-# OAuthPasswordBearerインスタンスであると同時に、oauth2_scheme(some, parameters)のように呼び出し可能なので
-# Depends(oauth2_scheme)と書くことができる
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -44,46 +43,90 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    crud.delete_user(db, db_user)
+    return {"user_id": user_id}
+
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
 def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 @app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
     print(token)
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
-@app.post("/token")
-def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
-    # OAuth2PasswordRequestForm には
-    db_user = crud.get_user_by_email(db, form_data.username)
-    if db_user is None:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    hashed_password = form_data.password + "notreallyhashed"
-    if hashed_password != db_user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    # tokenエンドポイントの戻り値はaccess_tokenとtoken_typeを含むdictである必要がある
-    # https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#return-the-token
-    return {"access_token": db_user.email, "token_type": "bearer"}
-
-
-
-def get_current_active_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    # tokenには "/token" でリターンした access_token が格納されている
-    email = token
+def authenticate_user(db: Session, email: str, password: str) -> Optional[models.User]:
+    """emailとpasswordで認証を行う"""
     db_user = crud.get_user_by_email(db, email)
     if db_user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    if not db_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        return None
+    print("---------------------------------")
+    print(db_user.email)
+    print(db_user.hashed_password)
+    print("---------------------------------")
+    print("---------------------------------")
+    print(email)
+    print(password)
+    print(hashed_password)
+    print("---------------------------------")
+    if not auth.verify_password(password, db_user.hashed_password):
+        return None
     return db_user
 
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
+    # tokenには "/token" でリターンした access_token が格納されている
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
+        email: str = payload["sub"]
+        if email is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(email=email)
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user_by_email(db, email=email)
+    if user is None:
+        raise credentials_exception
+    return user
 
-@app.get("/myuser", response_model=schemas.User)
+def get_current_active_user(current_user: models.User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@app.post("/token")
+def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    # OAuth2PasswordRequestForm は username, password, scope, grant_type といったメンバを持つ
+    # https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#oauth2passwordrequestform
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=auth.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth.create_access_token(
+        data={"sub": user.email},
+        expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/me", response_model=schemas.User)
 def read_users_me(current_user: models.User = Depends(get_current_active_user)):
     return current_user
+
+@app.get("/me/items", response_model=List[schemas.Item])
+def read_items_me(current_user: models.User = Depends(get_current_active_user)):
+    return current_user.items
