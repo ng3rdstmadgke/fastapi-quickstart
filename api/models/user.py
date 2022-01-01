127979cfc795435754e747eca3f060d825092eaa
keita.midorikawa <keita.midorikawa@beex-inc.com>
2022-01-02 00:29:01 +0900

    routerを移動

--- api/models/user.py
+++ api/models/user.py
@@ -5,10 +5,10 @@ from database import Base
 
 class User(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(191), unique=True, index=True)
     hashed_password = Column(String(191))
     is_active = Column(Boolean, default=True)
-    
+
     items = relationship("Item", back_populates="user")
