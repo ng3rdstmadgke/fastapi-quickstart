33978c701250ce58ea052273f0730747bc06f31c
keita.midorikawa <keita.midorikawa@beex-inc.com>
2022-01-02 12:22:38 +0900

    crud.pyを移動

--- api/cruds/user.py
+++ api/cruds/user.py
@@ -27,13 +27,3 @@ def delete_user(db: Session, user: models.User):
     db.delete(user)
     db.commit()
     return user
-
-def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Item).offset(skip).limit(limit).all()
-
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), user_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)  # dbに登録された内容をdb_itemに反映
-    return db_item
